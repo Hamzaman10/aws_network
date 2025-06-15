9e0ecf0a245cbcbea7fa0765cf2a37cffcba5531
EC2 Default User <ec2-user@ip-172-31-93-129.ec2.internal>
2025-06-15 22:16:04 +0000

    Adding my module

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "default_tags" {
+  default = {
+    "Owner" = "Hamza"
+    "App"   = "Web"
+  }
+  type = map(any)
+  description = "Default tags to be applied to all AWS resources"
+}
+
+variable "prefix" {
+  default = "week4"
+  type = string
+  description = "Name prefix"
+}
+
+variable "public_subnet_cidrs" {
+  default = ["10.20.0.0/24", "10.20.1.0/24"]
+  type = list(string)
+  description = "Public Subnet CIDRs"
+}
+
+variable "vpc_cidr" {
+  default = "10.20.0.0/16"
+  type = string
+  description = "VPC CIDR"
+}
+
+variable "env" {
+  default = "dev"
+  type = string
+  description = "Deployment Environment"
+}
