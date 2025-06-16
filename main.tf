553087577effd080421e8a0aba2892ad1a20541f
EC2 Default User <ec2-user@ip-172-31-93-129.ec2.internal>
2025-06-16 02:04:59 +0000

    Adding my module

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-#----------------------------------------------------------
-# ACS730 - Week 4 - Terraform Introduction
-#
-# Build Fault Tolerant Static Web Site
-#----------------------------------------------------------
-
 # Step 1 - Define the provider
 provider "aws" {
   region = "us-east-1"
@@ -14,34 +8,34 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-# Local variables to construct consistent names
 locals {
-  default_tags = merge(var.default_tags, { "Env" = var.env })
-  name_prefix  = "${var.prefix}-${var.env}"
+  default_tags = merge(
+    var.default_tags,
+    { "Env" = var.env }
+  )
 }
 
 # Create a new VPC 
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
-
   tags = merge(
     local.default_tags, {
-      Name = "${local.name_prefix}-vpc"
+      Name = "${var.prefix}-vpc"
     }
   )
 }
 
-# Provision public subnets in the custom VPC
-resource "aws_subnet" "public_subnet" {
-  count             = length(var.public_subnet_cidrs)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidrs[count.index]
-  availability_zone = data.aws_availability_zones.available.names[count.index]
 
+# Add provisioning of the public subnetin the default VPC
+resource "aws_subnet" "public_subnet" {
+  count             = length(var.private_cidr_blocks)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_cidr_blocks[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = merge(
     local.default_tags, {
-      Name = "${local.name_prefix}-public-subnet-${count.index}"
+      Name = "${var.prefix}-public-subnet-${count.index}"
     }
   )
 }
@@ -50,30 +44,28 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
-  tags = merge(
-    local.default_tags, {
-      Name = "${local.name_prefix}-igw"
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${var.prefix}-igw"
     }
   )
 }
 
-# Route table for public subnets
-resource "aws_route_table" "public_subnets" {
+# Route table to route add default gateway pointing to Internet Gateway (IGW)
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-
   tags = {
-    Name = "${local.name_prefix}-route-public-subnets"
+    Name = "${var.prefix}-route-public-route_table"
   }
 }
 
-# Associate subnets with the route table
-resource "aws_route_table_association" "public_routes" {
+# Associate subnets with the custom route table
+resource "aws_route_table_association" "public_route_table_association" {
   count          = length(aws_subnet.public_subnet[*].id)
-  route_table_id = aws_route_table.public_subnets.id
+  route_table_id = aws_route_table.public_route_table.id
   subnet_id      = aws_subnet.public_subnet[count.index].id
 }
